792f317bc503408ada774a0871fd56a58f059435
Carlos <carlosegalarzas@gmail.com>
2023-01-14 12:32:25 -0500

    empezando con git

---
+++ main.py
@@ -0,0 +1,7 @@
+nombre = input ("ingrese su nombre:")
+apellido = input ("ingrese su apellido:")
+edad = input ("ingrese su edad:")
+
+fullname = "Mi nombre es:   " + nombre +" Mi apellido es:   " + apellido + " y tengo una edad de:   " + edad
+print (fullname)
+
